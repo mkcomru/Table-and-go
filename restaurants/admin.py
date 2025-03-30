cb8a234d94e3f0cc010adf4a280834d7f2a898d4
mkcomru <grebenshchikov.my@students.dvfu.ru>
2025-03-30 23:17:28 +1000

    add Restaurant model and migrate it

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
-from .models import Cuisine
+from .models import Cuisine, Restaurant
 
 
 @admin.register(Cuisine)
 class CuisineAdmin(admin.ModelAdmin):
     list_display = ('name',)
     search_fields = ('name',)
+
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'phone', 'email')
+    list_filter = ['cuisines']
+    search_fields = ('name', 'address', 'description')
+    filter_horizontal = ['cuisines']
 
 
 
--- restaurants/models.py
+++ restaurants/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 
 class Cuisine(models.Model):
@@ -12,4 +13,39 @@ class Cuisine(models.Model):
         verbose_name = "Кухня"
         verbose_name_plural = "Кухни"
         ordering = ["name"]
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name="Название ресторана")
+    description = models.TextField(blank=True, null=True, verbose_name="Описание ресторана")
+    address = models.CharField(max_length=256, verbose_name="Адрес ресторана")
+    phone = models.CharField(max_length=10, verbose_name="Контактный телефон")
+    email = models.CharField(max_length=100, verbose_name="Почта")
+
+    latitude = models.DecimalField(
+        max_digits=9, decimal_places=6,
+        blank=True, null=True,
+        verbose_name="Широта"
+    )
+    longitude = models.DecimalField(
+        max_digits=9, decimal_places=6,
+        blank=True, null=True,
+        verbose_name="Долгота"
+    )
+
+    website_url = models.URLField(blank=True, null=True, verbose_name="Сайт ресторана")
+
+    cuisines = models.ManyToManyField(Cuisine, related_name="restourants", verbose_name="Типы кухни")
+
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата обновления")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Ресторан"
+        verbose_name_plural = "Рестораны"
+        ordering = ['-created_at']
+
 
