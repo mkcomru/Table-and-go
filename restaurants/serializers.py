9797a52a509a04e202f0abe126c108c6c5ddb855
mkcomru <grebenshchikov.my@students.dvfu.ru>
2025-04-07 17:40:58 +1000

    add new serializer for branches

--- restaurants/serializers.py
+++ restaurants/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Establishment, EstablishmentAdmin, BranchImage, Cuisine, Table, AdminInvitation, WorkingHours, Menu
+from .models import Establishment, Branch
 
 
 class EstablishmentListSerializer(serializers.ModelSerializer):
@@ -54,6 +54,80 @@ class EstablishmentListSerializer(serializers.ModelSerializer):
     def get_branches_count(self, obj):
         return obj.get_branches_count()
 
+
+class BranchListSerializer(serializers.ModelSerializer):
+    establishment_name = serializers.SerializerMethodField()
+    establishment_type = serializers.SerializerMethodField()
+    photo = serializers.SerializerMethodField()
+    rating = serializers.SerializerMethodField()
+    working_hours = serializers.SerializerMethodField()
+    tables_count = serializers.SerializerMethodField()
+    available_tables_count = serializers.SerializerMethodField()
+    cuisine_types = serializers.SerializerMethodField()
+    district = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Branch
+        fields = [
+            'id',
+            'name',
+            'address',
+            'district',
+            'phone',
+            'average_check',
+            'is_main',
+            'establishment_name',
+            'establishment_type',
+            'photo',
+            'rating',
+            'working_hours',
+            'tables_count',
+            'available_tables_count',
+            'cuisine_types'
+        ]
+
+    def get_rating(self, obj):
+        return obj.average_rating()
+
+    def get_establishment_name(self, obj):
+        return obj.establishment.name
+
+    def get_establishment_type(self, obj):
+        return obj.establishment.establishment_type
+    
+    def get_photo(self, obj):
+        return obj.get_main_image()
+    
+    def get_working_hours(self, obj):
+        hours = obj.working_hours.all().order_by('day_of_week')
+        result = []
+
+        for hour in hours:
+            if hour.is_closed:
+                status = "Выходной"
+            else:
+                status = f"{hour.opening_time.strftime('%H:%M')} - {hour.closing_time.strftime('%H:%M')}"
+        
+            result.append({
+                'day_of_week': hour.day_of_week,
+                'day_name': hour.get_day_of_week_display(),
+                'status': status,
+                'is_closed': hour.is_closed
+            })
+
+        return result
+    
+    def get_tables_count(self, obj):
+        return obj.table_count()
+    
+    def get_available_tables_count(self, obj):
+        return obj.get_available_tables.count()
+    
+    def get_cuisine_types(self, obj):
+        return [cuisine.name for cuisine in obj.establishment.cuisines.all()]
+    
+    def get_district(self, obj):
+        return obj.district.name
 
 
 
