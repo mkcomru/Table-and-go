20a5e84f15d0c718c9459b5b901060b8849af83a
mkcomru <grebenshchikov.my@students.dvfu.ru>
2025-07-26 22:47:10 +0900

    final changes

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -41,12 +41,16 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin', # pip install django-jazzmin
+    # 'django_daisy', pip install django-daisy
     'django.contrib.admin',
+    # 'django.contrib.humanize', pip install django-daisy
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_yasg', 
     'django.contrib.postgres',
     'rest_framework',
     'rest_framework_simplejwt',
